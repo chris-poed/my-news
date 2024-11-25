672cfa22234d4f1b4c71901b1aab17aefffa853e
chris-poed <christianpoed@gmail.com>
2024-11-25 14:02:30 +0000

    add test for GET api/topics

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -5,9 +5,16 @@ const db = require("../db/connection")
 const seed = require("../db/seeds/seed")
 const endpointsJson = require("../endpoints.json");
 
-/* Set up your test imports here */
 
 /* Set up your beforeEach & afterAll functions here */
+
+beforeEach(() => {
+  return seed(data)
+})
+
+afterAll(() => {
+  return db.end()
+})
 
 describe("GET /api", () => {
   test("200: Responds with an object detailing the documentation for each endpoint", () => {
@@ -18,4 +25,19 @@ describe("GET /api", () => {
         expect(endpoints).toEqual(endpointsJson);
       });
   });
-});
+})
+
+describe("GET /api/topics", () => {
+  test("200: Responds with an array of topic objects with slug and description properties", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body: { topics } }) => {
+        expect(topics.length).toBe(3)
+        topics.forEach((topic) => {
+          expect(typeof topic.slug).toBe("string")
+          expect(typeof topic.description).toBe("string")
+        })
+      })
+  })
+})
