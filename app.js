14fd2554fccadc412ae470b4c82a1e16019d6275
chris-poed <christianpoed@gmail.com>
2024-11-25 14:03:00 +0000

    add /api/topics endpoint and error handling

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express")
 const  { 
-    getApi
+    getApi,
+    getTopics
 } = require('./controllers/api.controller')
 
 const app = express()
 app.use(express.json())
 
 app.get('/api', getApi)
+app.get('/api/topics', getTopics)
+
+app.use((err, req, res, next) => {
+ if (err.status && err.msg) {
+    res.status(err.status).send({ msg: err.msg })
+ } else {
+    next(err)
+ }
+})
+app.use((err, req, res, next) => {
+    res.status(500).send({ msg: 'Internal server error'})
+})
 
 module.exports = app;
