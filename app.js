d2c2c378c2e5faad17a7cffafb6d1e6d3fcbb1f5
chris-poed <christianpoed@gmail.com>
2024-11-25 15:37:40 +0000

    add endpoint for GET /api/articles/article_id

--- app.js
+++ app.js
@@ -1,22 +1,35 @@
 const express = require("express")
 const  { 
     getApi,
-    getTopics
+    getTopics,
+    getArticle
 } = require('./controllers/api.controller')
 
 const app = express()
-app.use(express.json())
 
 app.get('/api', getApi)
 app.get('/api/topics', getTopics)
+app.get('/api/articles/:article_id', getArticle)
+
 
 app.use((err, req, res, next) => {
- if (err.status && err.msg) {
-    res.status(err.status).send({ msg: err.msg })
- } else {
-    next(err)
- }
+    console.log(err, "<---error in 1st app.use")
+    if (err.status && err.msg) {
+        res.status(err.status).send({ msg: err.msg })
+    } else {
+        next(err)
+    }
 })
+
+app.use((err, req, res, next) => {
+    console.log(err.code, "err.code in 2nd app.use")
+    if (err.code === "22P02") {
+        res.status(400).send({ msg: "Bad request" })
+    } else {
+        next(err)
+    }
+})
+
 app.use((err, req, res, next) => {
     res.status(500).send({ msg: 'Internal server error'})
 })
