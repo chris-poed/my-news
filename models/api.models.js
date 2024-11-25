c67f1cb49f5c8b80692c0aeaec0b941822622bbc
chris-poed <christianpoed@gmail.com>
2024-11-25 15:38:32 +0000

    add fetchArticle function

--- models/api.models.js
+++ models/api.models.js
@@ -5,3 +5,11 @@ exports.fetchTopics = () => {
         return rows
     })
 }
+exports.fetchArticle = (article_id) => {
+    return db.query(`SELECT * FROM articles WHERE article_id = $1`, [article_id]).then(({ rows }) => {
+        if (rows.length === 0) {
+            return Promise.reject({ status: 404, msg: 'Article_id does not exist'})
+        }
+        return rows[0];
+    })
+}
