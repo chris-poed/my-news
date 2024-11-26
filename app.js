5135b29d2dd879d4d88e5d9611afcf09b348a77c
chris-poed <christianpoed@gmail.com>
2024-11-26 15:56:31 +0000

    add /api/articles/:article_id/comments endpoint

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const  {
     getApi,
     getTopics,
     getArticle,
-    getArticles
+    getArticles,
+    getComments
 } = require('./controllers/api.controller')
 
 const { //endpointErrorHandler, 
@@ -18,12 +19,11 @@ app.get('/api', getApi)
 app.get('/api/topics', getTopics)
 app.get('/api/articles/:article_id', getArticle)
 app.get('/api/articles', getArticles)
+app.get('/api/articles/:article_id/comments', getComments)
 
 app.all("*", (req, res) => {
     res.status(404).send({ msg: "Endpoint not found" })
 })
-
-//app.all(endpointErrorHandler)
 app.use(postgresErrorHandler)
 app.use(customErrorHandler)
 app.use(serverErrorHandler)
