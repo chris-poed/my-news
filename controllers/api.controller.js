5fe101d3002906a184968714895b61f61622fed6
chris-poed <christianpoed@gmail.com>
2024-11-27 09:13:58 +0000

    refactor by moving some controllers to articles.controller

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,10 +1,6 @@
 const endpointsJSON = require('../endpoints.json')
 const {
     fetchTopics,
-    fetchArticle,
-    fetchArticles,
-    fetchComments,
-    checkArticleExists
 } = require('../models/api.models')
 
 exports.getApi = (req, res) => {
@@ -16,32 +12,4 @@ exports.getTopics = (req, res, next) => {
     .then((topics) => {
         res.status(200).send({ topics })
     })
-}
-
-exports.getArticle = (req, res, next) => {
-    const { article_id } = req.params
-    fetchArticle(article_id).then((article) => {
-        res.status(200).send({ article })
-    })
-    .catch(next)
-}
-
-exports.getArticles = (req, res, next) => {
-    fetchArticles().then((articles) => {
-        res.status(200).send({ articles })
-    })
-    .catch(next)
-}
-
-exports.getComments = (req, res, next) => {
-    const { article_id } = req.params
-    const promises = [fetchComments(article_id)]
-    if(article_id) {
-        promises.push(checkArticleExists(article_id))
-    }
-    Promise.all(promises)
-    .then(([comments]) => {
-        res.status(200).send({ comments })
-    })
-    .catch(next)
 }
