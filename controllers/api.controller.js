41f6ae2759f1f872b7015dd9d9378d52efc8ae5f
chris-poed <christianpoed@gmail.com>
2024-11-27 12:08:20 +0000

    move topics controller to topics.controller

--- controllers/api.controller.js
+++ controllers/api.controller.js
@@ -1,15 +1,6 @@
 const endpointsJSON = require('../endpoints.json')
-const {
-    fetchTopics,
-} = require('../models/api.models')
 
 exports.getApi = (req, res) => {
     res.status(200).send({ endpoints: endpointsJSON })
 }
 
-exports.getTopics = (req, res, next) => {
-    fetchTopics()
-    .then((topics) => {
-        res.status(200).send({ topics })
-    })
-}
