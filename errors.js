cea7612a38285ea81722fb69d0cdd9022398c7f4
chris-poed <christianpoed@gmail.com>
2024-11-28 10:15:01 +0000

    add error handling for POST /:article_id/comment

--- errors.js
+++ errors.js
@@ -1,9 +1,13 @@
 exports.postgresErrorHandler = (err, req, res, next) => {
     if (err.code === "22P02") {
         res.status(400).send({ msg: "Bad request" })
-    } else if (err.code === "23503") {
+    } else if (err.code === "23503" && err.detail.includes("articles")) {
         res.status(404).send({ msg: "Article_id does not exist" })
-    } else {
+    }else if (err.code === "23503" && err.detail.includes("users")) {
+        res.status(404).send({ msg: "Invalid username" })
+    } else if (err.code === "23502") {
+        res.status(422).send({ msg: "Unprocessable entity" })
+    }else {
         next(err)
     }
 }
