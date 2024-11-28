a1dc9b475bd856af8bb8f20f2de8e9bc63a04613
chris-poed <christianpoed@gmail.com>
2024-11-28 10:16:08 +0000

    add checkTopicExists model

--- models/utils.models.js
+++ models/utils.models.js
@@ -8,3 +8,16 @@ exports.checkArticleExists = (article_id) => {
         return rows;
     })
 }
+
+exports.checkTopicExists = (topic) => {
+    return db.query(`SELECT * FROM topics WHERE slug = $1`, [topic])
+    .then(({ rows }) => {
+        if (!rows.length) {
+            return Promise.reject({
+                status: 404,
+                msg: "Topic not found"
+            })
+        }
+        return rows
+    })
+}
